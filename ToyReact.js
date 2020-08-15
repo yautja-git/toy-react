11d0adede858476c0ae3f01fee49d5aa41110381
Naive <1344715366@qq.com>
2020-08-15 22:13:48 +0800

    实现简易的JSX

--- ToyReact.js
+++ ToyReact.js
@@ -1,14 +1,72 @@
-export let ToyReact={
-    createElement(type,attributes,...children){
-        let element=document.createElement(type)
-        for (const key in attributes) {
-            element.setAttribute(key,attributes[key])
-        }
-        for (const child of children) {
-            element.append(child)
-        }
-        console.log(element);
-        return element
+class ElementWrapper {
+    constructor(type) {
+        this.root = document.createElement(type)
     }
+    setAttribute(name, value) {
+        this.root.setAttribute(name, value)
+    }
+    appendChild(component) {
+        this.root.append(component.root)
+
+    }
+}
+
+class TextWrapper {
+    constructor(content) {
+        this.root = document.createTextNode(content)
+    }
+}
+
+export class Component {
+    constructor() {
+        this.props = Object.create(null);
+        this.children = [];
+        this._root = null
+    }
+    setAttribute(name, value) {
+        this.props[name] = value
+    }
+    appendChild(component) {
+        this.children.push(component)
+    }
+    get root() {
+        if (!this._root) {
+            this._root = this.render().root
+        }
+        return this._root
+    }
+}
+
+export function createElement(type, attributes, ...children) {
+    let element
+    if (typeof (type) === 'string') {
+        element = new ElementWrapper(type)
+    } else {
+        element = new type
+    }
+
+    for (const key in attributes) {
+        element.setAttribute(key, attributes[key])
+    }
+    let insertChildren = (children) => {
+        for (const child of children) {
+            if (typeof (child) === 'string') {
+                child = new TextWrapper(child)
+            }
+            if ((typeof (child) === 'object') && (child instanceof Array)) {
+                insertChildren(child)
+            }else{
+               element.appendChild(child) 
+            }
+        }
+    }
+    insertChildren(children)
+
+    console.log(element);
+    return element
+}
+
+export function render(component, parentElement) {
+    parentElement.appendChild(component.root)
 }
 
--- main.js
+++ main.js
@@ -1,12 +1,25 @@
 
-import { ToyReact } from './ToyReact'
+import { createElement,render,Component } from './ToyReact'
 
-class MyComponent { }
-
-let a = <div name='a' id='ida'>
-            <span></span>
-            <span></span>
-            <span></span>
+class MyComponent extends Component {
+    render(){
+        return <div>
+            <h1>my component</h1>
+            {this.children}
         </div>
+    }
+ }
 
-document.body.appendChild(a)
+// let a = <div name='a' id='ida'>
+//             <span>aaa</span>
+//             <span>22</span>
+//             <span>33</span>
+//         </div>
+
+let a = <MyComponent name='a' id='ida'>
+            <span>aaa</span>
+            <span>22</span>
+            <span>33</span>
+        </MyComponent>
+
+render(a,document.body)
